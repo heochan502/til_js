76a48e0cafb5682a94188763e154bb53b1e5091a
greenhc502 <greenhc502@gmail>
2025-05-02 13:17:27 +0900

    [docs] : js 초기문법

---
+++ js/lib.js
@@ -0,0 +1,17 @@
+//메시지를 출력하는 기능
+function 커피머신(_msg) {
+  console.log(_msg);
+}
+
+console.log("1. 안녕");
+
+이름표 = 123;
+console.log(이름표);
+var 이름표;
+
+let 홍길동;
+홍길동 = 333;
+console.log(홍길동);
+
+const 임꺽정 = 444;
+console.log(임꺽정);
